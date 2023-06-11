49ca4ec21fcd481b924da69c3680eb3c151a00ea
Agustín Bologna <agustin.bologna@gmail.com>
2023-06-11 18:18:25 -0300

    3D y camera movement

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
 #include "shaderClass.h"
+#include "Camera.h"
 using namespace std;
 
-
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main() {
 	glfwInit();
@@ -17,26 +23,28 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] =
-	{ //               COORDINATES                  /     COLORS           //
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+	{ // COORDINATES           // COLORS
+		-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,
+		-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,
+		 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,
+		 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,
+		 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f
 	};
 
 	// Indices for vertices order
 	GLuint indices[] =
 	{
-		0, 3, 5, // Lower left triangle
-		3, 2, 4, // Lower right triangle
-		5, 4, 1 // Upper triangle
+		0, 1, 2,
+		0, 2, 3,
+		0, 1, 4,
+		1, 2, 4,
+		2, 3, 4,
+		3, 0, 4
 	};
 
 	
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Raytracer pero del bueno", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Raytracer pero del bueno", NULL, NULL);
 	if (window == NULL) {
 		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
@@ -46,7 +54,7 @@ int main() {
 
 	gladLoadGL();
 
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram("default.vert", "default.frag");
 	
@@ -66,14 +74,23 @@ int main() {
 
 	GLuint timeID = glGetUniformLocation(shaderProgram.ID, "time");
 
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
+	
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.132f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
+
+		camera.Inputs(window);
+		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+
 		clock_t time = clock();
 		glUniform1f(timeID, time);
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
--- default.vert
+++ default.vert
@@ -2,9 +2,12 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
+// Inputs the matrices needed for 3D view
+uniform mat4 camMatrix;
+
 out vec3 color;
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   gl_Position = camMatrix * vec4(aPos, 1.0);
    color = aColor;
 };
