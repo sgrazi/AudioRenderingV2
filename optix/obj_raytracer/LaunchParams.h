e26e2b169930b97380e8b51382e4130eee040270
Stefano Graziani <stefa99graziani@gmail.com>
2023-07-08 18:55:40 -0300

    se manda todo

---
+++ optix/obj_raytracer/LaunchParams.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "gdt/math/vec.h"
+#include "optix7.h"
+
+using namespace gdt;
+
+struct TriangleMeshSBTData
+{
+    vec3f color;
+    vec3f *vertex;
+    vec3i *index;
+};
+
+struct LaunchParams
+{
+    struct
+    {
+        uint32_t *colorBuffer;
+        vec2i size;
+    } frame;
+
+    struct
+    {
+        vec3f position;
+        vec3f direction;
+        vec3f horizontal;
+        vec3f vertical;
+    } camera;
+
+    OptixTraversableHandle traversable;
+};
+
