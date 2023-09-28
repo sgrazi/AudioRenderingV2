7fcb5c79aeb4b51d8ed04f7c4366f9da6105dd2d
Stefano Graziani <87818192+sgrazi@users.noreply.github.com>
2023-09-27 22:18:46 -0300

    Add transmitter to screen (#7)
    * WIP Add transmitter to screen
    
    * Set receiver optix (#6)
    
    * WIP initial placeReceiver implementation
    
    * updated gitignore
    
    * add new objects and updated cmakelist
    
    * Fix sphere obj
    
    * Set key to set receiver
    
    ---------
    
    Co-authored-by: AgusdosSantos <adossantos30@hotmail.com>
    Co-authored-by: Agustín Bologna <agustin.bologna@gmail.com>
    
    * update transmitter in audio render
    
    * Squashed commit of the following:
    
    commit 5c6468cbc8997d55a37fcf70c36eea50dfc47907
    Author: Stefano Graziani <stefa99graziani@gmail.com>
    Date:   Wed Sep 20 22:36:09 2023 -0300
    
        fix tiny_obj
    
    commit 354038fd00982b21cfffb26799b5562e32e469f3
    Author: Agustín Bologna <agustin.bologna@gmail.com>
    Date:   Tue Sep 19 21:39:14 2023 -0300
    
        updated cmake
    
    commit 127e99588e072443bfcb3ec236aa163328a79cee
    Author: Agustín Bologna <agustin.bologna@gmail.com>
    Date:   Tue Sep 19 21:24:28 2023 -0300
    
        push assets
    
    commit dad2e6e12e37dadffda5e6ec2a46e6f35eb3e0c4
    Author: Agustín Bologna <agustin.bologna@gmail.com>
    Date:   Tue Sep 19 21:23:53 2023 -0300
    
        updated gitignore
    
    commit ea38b7e42811df2fca43409726122cec12b1a3e8
    Merge: eed0e91 4fb4d09
    Author: Stefano Graziani <stefa99graziani@gmail.com>
    Date:   Sun Sep 10 16:45:01 2023 -0300
    
        Merge branch 'develop' of https://github.com/sgrazi/AudioRenderingV2 into CleanWorkspace
    
    commit 4fb4d09bfaa4cf5de7bbc8dedb060e49b509730e
    Author: Stefano Graziani <87818192+sgrazi@users.noreply.github.com>
    Date:   Sun Sep 10 16:00:20 2023 -0300
    
        Pathtracing (#3)
    
        * wip, pathtracer
    
        * loading materials, finished material hit case
    
        * pathtracer
    
        * WIP compile pathtraicing
    
        * fixed material absorption vector
    
        * improved absorption loading, fixed pathtracer
    
        * compiling fixes
    
        ---------
    
        Co-authored-by: AgusdosSantos <adossantos30@hotmail.com>
    
    commit cc791edeee70375813b63b2b10ec258d49ef8d29
    Author: AgusdosSantos <90009938+AgusdosSantos@users.noreply.github.com>
    Date:   Tue Aug 22 20:01:53 2023 -0300
    
        Optix MVP (#2)
    
        Co-authored-by: Stefano Graziani <stefa99graziani@gmail.com>
        Co-authored-by: Agustín Bologna <agustin.bologna@gmail.com>
    
    commit 471702044b65f7222ff4bf74dbd1c1f490af916c
    Author: Stefano Graziani <stefa99graziani@gmail.com>
    Date:   Sat May 6 17:44:22 2023 -0300
    
        ahora si libs
    
    commit 7bb728b061c17b317b38eca1ae9876a61a61eb73
    Merge: 0cf0bd8 38c40a7
    Author: Stefano Graziani <stefa99graziani@gmail.com>
    Date:   Sun Apr 30 18:08:20 2023 -0300
    
        Merge branch 'develop' of https://github.com/sgrazi/AudioRenderingV2 into develop
    
    commit 0cf0bd83f96c471ef63e7a5a6881594767f97035
    Author: Stefano Graziani <stefa99graziani@gmail.com>
    Date:   Sun Apr 30 18:07:51 2023 -0300
    
        removed docs
    
    commit 38c40a7c864cb7b092799c99fba0c73ed0b7f64a
    Author: Agustín Bologna <agustin.bologna@gmail.com>
    Date:   Sat Apr 29 13:48:34 2023 -0300
    
        Testing with audio file
    
    commit 50b644a62a38d962b90c494d7eb0cd1c4e2b1a59
    Author: Agustín Bologna <agustin.bologna@gmail.com>
    Date:   Sat Apr 22 18:39:14 2023 -0300
    
        develop branch, merging both modes
    
    * Tiny fix
    
    * tiny fix 2
    
    ---------
    
    Co-authored-by: AgusdosSantos <adossantos30@hotmail.com>
    Co-authored-by: Agustín Bologna <agustin.bologna@gmail.com>
    Co-authored-by: AgusdosSantos <90009938+AgusdosSantos@users.noreply.github.com>

--- prebuild/obj_raytracer/OptixModel.cpp
+++ prebuild/obj_raytracer/OptixModel.cpp
@@ -1,7 +1,8 @@
 #include "OptixModel.h"
 #include "AudioRenderer.h"
+#define TINYOBJLOADER_IMPLEMENTATION
 #include "3rdParty/tiny_obj_loader.h"
-#include "tinyxml2.h"
+// #include "tinyxml2.h"
 // std
 #include <set>
 #include <map>
@@ -71,8 +72,8 @@ int addVertex(TriangleMesh *mesh,
     return newID;
 }
 
-// OptixModel *loadOBJ(const std::string &objFile, tinyxml2::XMLDocument &xml_dict)
-OptixModel *loadOBJ(const std::string &objFile, int &xml_dict)
+// OptixModel *loadOBJ(const std::string &objFile, tinyxml2::XMLDocument &xml_dic)
+OptixModel *loadOBJ(const std::string &objFile)
 {
     OptixModel *model = new OptixModel;
 
@@ -131,8 +132,7 @@ OptixModel *loadOBJ(const std::string &objFile, int &xml_dict)
                 mesh->diffuse = gdt::randomColor(materialID);
                 if (materialID >= 0)
                 {
-                    // mesh->material_absorption = get_absorption(materialID, xml_dict);
-                    mesh->material_absorption = 0.5f;
+                    mesh->material_absorption = 0;//get_absorption(materialID, xml_dict);
                 }
             }
 
@@ -163,66 +163,64 @@ OptixModel *loadOBJ(const std::string &objFile, int &xml_dict)
 //     }
 // }
 
-// void placeCamera(OptixModel *model, vec3f cameraPosition)
-//{ // this does not actually place the sphere in cameraPosition, TO DO
-//     const std::string objFile = "../models/sphere.obj";
-//     const std::string mtlDir = objFile.substr(0, objFile.rfind('/') + 1);
-//     printf("%s", mtlDir.c_str());
-//     tinyobj::attrib_t attributes;
-//     std::vector<tinyobj::shape_t> shapes;
-//     std::vector<tinyobj::material_t> materials;
-//     std::string err = "";
-//
-//     bool readOK = tinyobj::LoadObj(&attributes,
-//                                    &shapes,
-//                                    &materials,
-//                                    &err,
-//                                    &err,
-//                                    objFile.c_str(),
-//                                    mtlDir.c_str(),
-//                                    /* triangulate */ true);
-//
-//     if (!readOK)
-//     {
-//         throw std::runtime_error("Could not read sphere OBJ model from " + objFile + " : " + err);
-//     }
-//
-//     if (materials.empty())
-//         throw std::runtime_error("could not parse materials ...");
-//
-//     std::set<int> materialIDs;
-//     for (auto faceMatID : shapes[0].mesh.material_ids)
-//     {
-//         materialIDs.insert(faceMatID);
-//     }
-//
-//     for (int materialID : materialIDs)
-//     {
-//         std::map<tinyobj::index_t, int> knownVertices;
-//         TriangleMesh *mesh = new TriangleMesh();
-//
-//         for (int faceID = 0; faceID < shapes[0].mesh.material_ids.size(); faceID++)
-//         {
-//             if (shapes[0].mesh.material_ids[faceID] != materialID)
-//                 continue;
-//             tinyobj::index_t idx0 = shapes[0].mesh.indices[3 * faceID + 0];
-//             tinyobj::index_t idx1 = shapes[0].mesh.indices[3 * faceID + 1];
-//             tinyobj::index_t idx2 = shapes[0].mesh.indices[3 * faceID + 2];
-//
-//             vec3i idx(addVertex(mesh, attributes, idx0, knownVertices),
-//                       addVertex(mesh, attributes, idx1, knownVertices),
-//                       addVertex(mesh, attributes, idx2, knownVertices));
-//             mesh->index.push_back(idx);
-//             mesh->diffuse = (const vec3f &)materials[materialID].diffuse;
-//             mesh->diffuse = gdt::randomColor(materialID);
-//             if (materialID >= 0)
-//             {
-//                 mesh->materialID = materialID;
-//             }
-//         }
-//         if (mesh->vertex.empty())
-//             delete mesh;
-//         else
-//             model->meshes.push_back(mesh);
-//     }
-// }
+void placeReceiver(Sphere sphere, OptixModel *model, vec3f cameraPosition)
+{
+    std::set<int> uniqueValues;
+    // moving the sphere
+    for (const auto& shape : sphere.shapes) {
+        // shape.mesh.indices contains repeated indexes due to the shapes sharing indexes
+        // this "for" will make sure that the same index is not overwritten multiple times
+        for (const auto& num : shape.mesh.indices) {
+            if (uniqueValues.find(num.vertex_index) == uniqueValues.end()) {
+                uniqueValues.insert(num.vertex_index);
+            }
+        }
+        for (const auto& index : uniqueValues) {
+
+            // Translate each vertex by (x, y, z)
+            sphere.attributes.vertices[3 * index + 0] = cameraPosition.x + sphere.original_attributes.vertices[3 * index + 0];
+            sphere.attributes.vertices[3 * index + 1] = cameraPosition.y + sphere.original_attributes.vertices[3 * index + 1];
+            sphere.attributes.vertices[3 * index + 2] = cameraPosition.z + sphere.original_attributes.vertices[3 * index + 2];
+
+        }
+    }
+
+    std::set<int> materialIDs;
+    for (auto faceMatID : sphere.shapes[0].mesh.material_ids)
+    {
+        materialIDs.insert(faceMatID);
+    }
+
+    // converting to optix model
+    for (int materialID : materialIDs)
+    {
+        std::map<tinyobj::index_t, int> knownVertices;
+        TriangleMesh* mesh = new TriangleMesh();
+
+        for (int faceID = 0; faceID < sphere.shapes[0].mesh.material_ids.size(); faceID++)
+        {
+            if (sphere.shapes[0].mesh.material_ids[faceID] != materialID)
+                continue;
+            tinyobj::index_t idx0 = sphere.shapes[0].mesh.indices[3 * faceID + 0];
+            tinyobj::index_t idx1 = sphere.shapes[0].mesh.indices[3 * faceID + 1];
+            tinyobj::index_t idx2 = sphere.shapes[0].mesh.indices[3 * faceID + 2];
+
+            vec3i idx(addVertex(mesh, sphere.attributes, idx0, knownVertices),
+                    addVertex(mesh, sphere.attributes, idx1, knownVertices),
+                    addVertex(mesh, sphere.attributes, idx2, knownVertices));
+            mesh->index.push_back(idx);
+            // TO DO: check to see if we can remove this, we no longer need visuals
+            mesh->diffuse = (const vec3f&)sphere.materials[materialID].diffuse;
+            mesh->diffuse = gdt::randomColor(materialID);
+            if (materialID >= 0) {
+                mesh->material_absorption = materialID;
+            }
+        }
+        if (mesh->vertex.empty()) {
+            delete mesh;
+        } else {
+            model->meshes.pop_back();
+            model->meshes.push_back(mesh);
+        }
+    }
+}   
--- prebuild/obj_raytracer/main.cpp
+++ prebuild/obj_raytracer/main.cpp
@@ -17,13 +17,30 @@
 #include "RtAudio.h"
 #include "OptixModel.h"
 #include "AudioRenderer.h"
+#include <thread>
+#include <filesystem>
 // #include "tinyxml2.h"
 
 using namespace std;
 
 const unsigned int width = 1366;
 const unsigned int height = 768;
-float *volumen = new float(1.0f);
+
+float* volumen = new float(1.0f);
+std::string filePath = "../../assets/models/test.obj";
+vector<Mesh> objects;
+vector<Mesh> transmitterVector;
+
+Camera camera(width, height, glm::vec3(4.0f, 4.0f, 4.0f));
+
+// Create Optix mesh of same .obj
+OptixModel* scene = loadOBJ(filePath);
+
+// AudioRenderer
+// TODO modificar cuando se tenga comunicacion entre threads
+AudioRenderer* renderer = new AudioRenderer(scene, 256, 256);
+glm::ivec2 frameSize(width, height);
+Sphere sphere = Sphere();
 
 struct AudioInfo
 {
@@ -88,17 +105,58 @@ void audio(RtAudio *dac)
 	audioPlay(dac);
 }
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-	if (action == GLFW_RELEASE)
-		return; // only handle press events
-	if (key == GLFW_KEY_V)
+void setTransmitter (glm::vec3 posTransmitter) {
+	std::string transmitterPath = "../../assets/models/sphere.obj";
+	objl::Loader loader;
+	bool load_res = loader.LoadFile(transmitterPath);
+
+	if (load_res)
 	{
+		for (int i = 0; i < loader.LoadedMeshes.size(); i++) {
+			objl::Mesh mesh = loader.LoadedMeshes.at(i);
+			vector<Vertex> vertices;
+			vector<unsigned int> indices;
+			for (int j = 0; j < mesh.Vertices.size(); j++) {
+				Vertex vertex;
+				vertex.position = glm::vec3(mesh.Vertices.at(j).Position.X + posTransmitter.x, mesh.Vertices.at(j).Position.Y + posTransmitter.y, mesh.Vertices.at(j).Position.Z + posTransmitter.z);
+				vertex.normal = glm::vec3(mesh.Vertices.at(j).Normal.X, mesh.Vertices.at(j).Normal.Y, mesh.Vertices.at(j).Normal.Z);
+				vertex.color = glm::vec3(mesh.MeshMaterial.Kd.X, mesh.MeshMaterial.Kd.Y, mesh.MeshMaterial.Kd.Z);
+				vertices.push_back(vertex);
+			}
+			for (int j = 0; j < mesh.Indices.size(); j++) {
+				indices.push_back(mesh.Indices.at(j));
+			}
+			Mesh transmitter(vertices, indices);
+			transmitterVector.push_back(transmitter);
+		}
+	}
+	else { // error
+		cout << "Failed to transmitter OBJ" << endl;
+		throw new exception("B");
+	}
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_RELEASE) return; //only handle press events
+	if (key == GLFW_KEY_V) {
+		renderer->render();
+		renderer->isHit();
 		if (*volumen == 0.0f)
 			*volumen = 1.0f;
 		else
 			*volumen = 0.0f;
 		cout << "volumen seteado a " << *volumen << endl;
+	}
+	if (key == GLFW_KEY_E) {
+		transmitterVector.pop_back();
+		setTransmitter(glm::vec3(camera.Position.x, camera.Position.y, camera.Position.z));
+		renderer->setPos(glm::vec3(camera.Position.x, camera.Position.y, camera.Position.z));
+		cout << "transmitter set"  << endl;
+	}
+	if (key == GLFW_KEY_R) {
+		placeReceiver(sphere, scene, gdt::vec3f(camera.Position.x, camera.Position.y, camera.Position.z));
+		cout << "Receivcer set in Optix at: " << camera.Position.x << ", " << camera.Position.y << ", " << camera.Position.z << endl;
 	}
 }
 
@@ -129,8 +187,7 @@ void screen()
 	// Load obj && initialize Loader
 	objl::Loader loader;
 	bool load_res = loader.LoadFile(filePath);
-
-	vector<Mesh> objects;
+	setTransmitter(glm::vec3(0 , 0, 0));
 	vector<Mesh> lights;
 	if (load_res)
 	{
@@ -173,7 +230,7 @@ void screen()
 	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
 
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 0.0f));
+	// Camera camera(width, height, glm::vec3(0.0f, 0.0f, 0.0f));
 
 	// load material properties
 	// tinyxml2::XMLDocument doc;
@@ -181,15 +238,14 @@ void screen()
 	// {
 	// 	throw std::runtime_error("Failed to load material XML file");
 	// }
-	int doc;
 
-	// Create Optix mesh of same .obj
-	OptixModel *scene = loadOBJ(filePath, doc);
+	// // Create Optix mesh of same .obj
+	// OptixModel *scene = loadOBJ(filePath, doc);
 
 	// AudioRenderer
 	// TODO modificar cuando se tenga comunicacion entre threads
-	AudioRenderer *renderer = new AudioRenderer(scene, 256, 256);
-	glm::ivec2 frameSize(width, height);
+	// AudioRenderer *renderer = new AudioRenderer(scene, 256, 256);
+	// glm::ivec2 frameSize(width, height);
 	renderer->setThresholds(100.0, 0.1);
 	renderer->setPos(glm::vec3(0.f));
 	renderer->render();
@@ -211,6 +267,8 @@ void screen()
 
 		for (int i = 0; i < objects.size(); i++)
 			objects.at(i).Draw(shaderProgram, camera);
+
+		transmitterVector.back().Draw(shaderProgram, camera);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
