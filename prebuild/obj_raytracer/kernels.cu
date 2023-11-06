28edcf8d4d6972f2d54c3acc18c979440b7171de
AgusdosSantos <adossantos30@hotmail.com>
2023-11-06 20:00:22 -0300

    Fix convolute finally

--- prebuild/obj_raytracer/kernels.cu
+++ prebuild/obj_raytracer/kernels.cu
@@ -134,14 +134,14 @@ __global__ void load_complex_vector(cufftComplex* complex_data, float* real_vect
 __global__ void load_sample_segment(int second, unsigned int sampleRate, unsigned int segment_size_in_seconds, float* segment, float* samples) {
     // loads one second of samples and (segment_size_in_seconds - 1) seconds of zeros
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    int index = idx + second * sampleRate;
+    int sampleIndex = idx + second * sampleRate;
     if (idx < sampleRate * segment_size_in_seconds){
         if (idx < sampleRate) {
-                segment[index] = samples[index];
-            }
-            else {
-                segment[index] = 0.0f;
-            }
+            segment[idx] = samples[sampleIndex];
+        }
+        else {
+            segment[idx] = 0.0f;
+        }
     }
 }
 
@@ -149,38 +149,10 @@ __global__ void multiply_samples_segment_and_ir(int second, unsigned int sampleR
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     if (idx < sampleRate * segment_size_in_seconds){
         // (a + ib) (c + id) = (ac – bd) + i(ad + bc)
-        atomicAdd(&sampleData[idx].x, sampleData[idx].x * IRData[idx].x - sampleData[idx].y * IRData[idx].y);
-        atomicAdd(&sampleData[idx].y, sampleData[idx].x * IRData[idx].y + sampleData[idx].y * IRData[idx].x);
+        cufftComplex temp = sampleData[idx];
+        sampleData[idx].x = temp.x * IRData[idx].x - temp.y * IRData[idx].y;
+        sampleData[idx].y = temp.x * IRData[idx].y + temp.y * IRData[idx].x;
     }
-}
-
-void write_complex_to_file(cufftComplex* d_vector, int len, std::string filename) {
-    cufftComplex* h_vector = new cufftComplex[len];  // Host (CPU) buffer
-    
-    // Copy the data from device to host
-    cudaMemcpy(h_vector, d_vector, len * sizeof(cufftComplex), cudaMemcpyDeviceToHost);
-    
-    std::string realFilename = filename + "_r.txt";
-    std::string imagFilename = filename + "_i.txt";
-    
-    std::ofstream realFile(realFilename);
-    std::ofstream imagFile(imagFilename);
-    
-    if (!realFile.is_open() || !imagFile.is_open()) {
-        std::cerr << "Error opening one or both files." << std::endl;
-        delete[] h_vector;  // Don't forget to free the allocated host memory
-        return;
-    }
-
-    for (int i = 0; i < len; i++) {
-        realFile << h_vector[i].x << "\n"; // Write the real part to the real file
-        imagFile << h_vector[i].y << "\n"; // Write the imaginary part to the imaginary file
-    }
-
-    realFile.close();
-    imagFile.close();
-
-    delete[] h_vector;  // Free the allocated host memory
 }
 
 __global__ void add_segment_to_result_buffer(int second, int sampleRate, int segmentLen, float* segment, float* output) {
@@ -192,33 +164,7 @@ __global__ void add_segment_to_result_buffer(int second, int sampleRate, int seg
     }
 }
 
-void convolute_fourier_in_gpu(float* samples, float* IR_2, unsigned int samples_len, unsigned int sample_rate, unsigned int ir_len_2, float* outputBuffer) {
-    /// start 
-    unsigned int ir_len = 2048;
-    std::ifstream inputFile("C:\\Users\\stefa\\Documentos\\AudioRenderingV2\\build\\obj_raytracer\\ir_1D.txt"); // Replace "your_file.txt" with your file name
-    if (!inputFile.is_open()) {
-        std::cerr << "Failed to open the file." << std::endl;
-    }
-    std::vector<float> h_IR;
-    std::string line;
-    int i = 0;
-    while (i < ir_len && std::getline(inputFile, line)) {
-        i++;
-        std::stringstream ss(line);
-        float num;
-
-        while (ss >> num) {
-            h_IR.push_back(num);
-        }
-    }
-    inputFile.close();
-    // Allocate memory on the GPU
-    float *IR;
-    cudaMalloc((void**)&IR, h_IR.size() * sizeof(float));
-    // Copy data from the host to the device
-    cudaMemcpy(IR, h_IR.data(), h_IR.size() * sizeof(float), cudaMemcpyHostToDevice);
-    /// end
-    
+void convolute_fourier_in_gpu(float* samples, float* IR, unsigned int samples_len, unsigned int sample_rate, unsigned int ir_len, float* outputBuffer) {
     const int threadsPerBlock = 256;
     int blocks;
     const int batchSize = 1; // Number of batches 
@@ -250,8 +196,6 @@ void convolute_fourier_in_gpu(float* samples, float* IR_2, unsigned int samples_
     cudaDeviceSynchronize();
     CHCK_CUFFT_RES(cufftExecR2C(IRPlan, IR, IRData));
 
-    write_complex_to_file(IRData, ir_len, "fftir");
-
     // Finally convolute, second by second
     /*
     Basically we take each segment and we prolong it with 0's
@@ -275,16 +219,14 @@ void convolute_fourier_in_gpu(float* samples, float* IR_2, unsigned int samples_
         cudaDeviceSynchronize();
     }
 
-    printf("convolucion hecha\n");
-    
     // Clean up
     cufftDestroy(segmentPlan);
     cufftDestroy(IRPlan);
     cufftDestroy(inversePlan);
+    cudaFree(sampleSegment);
     cudaFree(segmentData);
     cudaFree(IRData);
     cudaFree(resultData);
-    cudaFree(IR); //DELETE ME TEMP ONLY
 }
 
 void copy_from_gpu(float* device_pointer, float* host_pointer, size_t size) {
