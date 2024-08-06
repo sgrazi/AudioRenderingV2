e299698e84c0bf16b404b21e09614f1519e96cee
Agustín Bologna <agustin.bologna@gmail.com>
2024-08-06 15:15:21 -0300

    Remove unused code from sphere

--- prebuild/obj_raytracer/Sphere.cpp
+++ prebuild/obj_raytracer/Sphere.cpp
@@ -4,34 +4,6 @@ Sphere::Sphere(HalfSphere *left_side, HalfSphere *right_side) {
 
     this->left_side = left_side;
     this->right_side = right_side;
-
-    // const std::string objFile = "../../assets/models/sphere.obj";
-    // const std::string mtlDir = objFile.substr(0, objFile.rfind('/'));
-    // tinyobj::attrib_t attributes;
-    // std::vector<tinyobj::shape_t> shapes;
-    // std::vector<tinyobj::material_t> materials;
-    // std::string err = "";
-
-    // bool readOK = tinyobj::LoadObj(&attributes,
-    //                                &shapes,
-    //                                &materials,
-    //                                &err,
-    //                                &err,
-    //                                objFile.c_str(),
-    //                                mtlDir.c_str(),
-    //                                /* triangulate */ true);
-    // if (!readOK)
-    // {
-    //     throw std::runtime_error("Could not read sphere OBJ model from " + objFile + " : " + err);
-    // }
-
-    // if (materials.empty())
-    //     throw std::runtime_error("could not parse materials ...");
-
-    // this->attributes = attributes;
-    // this->materials = materials;
-    // this->original_attributes = attributes;
-    // this->shapes = shapes;
 }
 
 HalfSphere Sphere::get_left_side() 
